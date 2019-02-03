ebce045a29607b74085710be8ce4dc4addd0a9e9
Musa Musa <musabrillz@gmail.com>
2019-02-03 11:10:47 +0100

    Created the contestant model and function to add constestant

--- contracts/Contest.sol
+++ contracts/Contest.sol
@@ -3,12 +3,22 @@ pragma solidity ^0.5.0;
 //Creates the Contract
 contract Contest {
 
-	//Class variables
-	string public contestant;
-
-	//Constructor
-	constructor () public {
-		contestant = "Tom";
+	//creating structure to model the contestant
+	struct Contestant {
+		uint id;
+		string name;
+		uint voteCount;
 	}
 
+	//use mapping to get or fetch the contestant details
+	mapping(uint => contestant) public contestants;
+
+	//add a public state variable to keep track of constestant count
+	uint public contestantCount;
+
+	//Adds a constestant
+	function addContestant (string _name) private {
+		contestantCount++
+		contestants[contestantCount] = Contestant(contestantCount, _name, 0);
+	}
 }
