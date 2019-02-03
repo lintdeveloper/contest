3661df9271a103965b2daa7537f429bab6f9c06d
Musa Musa <musabrillz@gmail.com>
2019-02-03 10:58:33 +0100

    Created First Smart contract

---
+++ contracts/Contest.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+//Creates the Contract
+contract Contest {
+
+	//Class variables
+	string public contestant;
+
+	//Constructor
+	constructor () public {
+		contestant = "Tom";
+	}
+
+}
